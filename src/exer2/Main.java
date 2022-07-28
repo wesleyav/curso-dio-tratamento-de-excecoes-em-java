cf09e028c20f043c7c3e8049c78119e8671d56be
Wesley Araújo Vieira <wesley.contato@hotmail.com>
2022-07-28 15:51:26 -0300

    Exercício 2 - parte 2

--- src/exer2/Main.java
+++ src/exer2/Main.java
@@ -3,16 +3,32 @@ package exer2;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 
+import javax.swing.JOptionPane;
+
 public class Main {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 
 		String nomeDoArquivo = "texto.txt";
-		imprimirArquivoNoConsole(nomeDoArquivo);
+		try {
+			imprimirArquivoNoConsole(nomeDoArquivo);
+		} catch (FileNotFoundException e) {
+			JOptionPane.showMessageDialog(null,
+					"Arquivo não encontrado. Revise a localização do arquivo" + e.getCause());
+//			e.printStackTrace();
+
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(null,
+					"Ocorreu um erro inesperado, entre em contato com o suporte " + e.getCause());
+			e.printStackTrace();
+		} finally {
+			System.out.println("Chegou no finally...");
+		}
 
 		System.out.println("Apesar da exception ou não, o programa continua...");
 	}
