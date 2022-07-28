369894f01ea2db71c901a5a54caa47b310663d82
Wesley AV <13851280+wesleyav@users.noreply.github.com>
2022-07-28 15:29:41 -0300

    Exercício 2 - parte 1

---
+++ src/exer2/Main.java
@@ -0,0 +1,39 @@
+package exer2;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		String nomeDoArquivo = "texto.txt";
+		imprimirArquivoNoConsole(nomeDoArquivo);
+
+		System.out.println("Apesar da exception ou não, o programa continua...");
+	}
+
+	private static void imprimirArquivoNoConsole(String nomeDoArquivo) throws IOException {
+
+		File file = new File(nomeDoArquivo);
+
+		BufferedReader br = new BufferedReader(new FileReader(file.getName()));
+		String line = br.readLine();
+
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		do {
+			bw.write(line);
+			bw.newLine();
+			line = br.readLine();
+
+		} while (line != null);
+		bw.flush();
+		br.close();
+	}
+
+}
